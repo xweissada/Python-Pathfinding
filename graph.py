39ee2bbb695b7b820be0701b9fc3979132c46324
weissada <weissada@fit.cvut.cz>
2021-07-17 22:25:40 +0200

    Graph class base structure

---
+++ graph.py
@@ -0,0 +1,9 @@
+from node import Node
+
+class Graph:
+
+    def __init__ ( self ):
+        nodes = []
+
+    def AddNode ( self, node ):
+        self . nodes . append ( node )
--- node.py
+++ node.py
@@ -1,6 +1,6 @@
 class Node:
 
-    # Initialise the node with its coordinates, set the state to unknown and create an empty list with neighbors
+    # Initialise the node with its coordinates, set the state to unknown and create an empty neighbors list
     def __init__ ( self, x=0, y=0 ):
         self . x = x
         self . y = y
@@ -24,17 +24,3 @@ class Node:
         for neighbor in self . neighbors:
             print ( neighbor, end = " " )
         print ()
-
-    
-test = Node ( 5, 7 )
-print ( test )
-
-print ( test . state )
-test . ChangeState ( 5 )
-print ( test . state )
-
-test2 = Node ( 0, 0 )
-test3 = Node ( 3, 5 )
-test . AddNeighbor ( test2 )
-test . AddNeighbor ( test3 )
-test . PrintNeighbors ()
