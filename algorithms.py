eef1b63609b87b5d6dac3336c9f8b9285881233b
weissada <weissada@fit.cvut.cz>
2021-07-18 11:46:20 +0200

    Added depth to BFS

--- algorithms.py
+++ algorithms.py
@@ -4,14 +4,16 @@ from graph import Graph
 def BFS ( graph, start ):
     for node in graph . nodes:
         node . ChangeState ( 0 )
+        node . depth = 0
     queue = []
     queue . append ( start )
     start . ChangeState ( 1 )
     while queue:
         vertice = queue . pop(0)
-        print ( vertice )
+        print ( str ( vertice . depth ) + " " + str ( vertice ) )
         for neighbor in vertice . neighbors:
             if neighbor . state == 0:
                 neighbor . ChangeState ( 1 )
+                neighbor . depth = vertice . depth + 1
                 queue . append ( neighbor )
         vertice . ChangeState ( 2 )
--- node.py
+++ node.py
@@ -5,6 +5,7 @@ class Node:
         self . x = x
         self . y = y
         self . state = 0
+        self . depth = 0
         self . neighbors = []
 
     # Return the Node as its coordinates
